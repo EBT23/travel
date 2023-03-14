131614ee0e0d7d9c8211071fa29909ff21d4b598
aderohmatmaulana98 <aderohmatmaulana77@gmail.com>
2023-03-14 23:28:42 +0700

    coba response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
 {
-				public function login()
-				{
-								$data['title'] = 'Login';
-								return view('Auth.login', $data);
-				}
+	public function login()
+	{
+		$data['title'] = 'Login';
 
-				public function register()
-				{
-								$data['title'] = 'Register';
-								return view('Auth.register', $data);
-				}
+		$client = new Client();
+
+		$response = $client->request('GET', 'https://travel.dlhcode.com/api/kota');
+		$data = json_decode($response->getBody(), true);
+
+		dd($data);
+
+		return view('Auth.login', $data);
+	}
+
+	public function register()
+	{
+		$data['title'] = 'Register';
+		return view('Auth.register', $data);
+	}
 }
